45fde46a16508d0be7e7946e6407f9f0a3fa31be
TPL <trphloc@gmail.com>
2017-05-15 20:16:15 +0700

    - viết ghi chú để hình dung việc chuyển trạng thái của activity

--- app/src/main/java/com/ygaps/demoactivity/MainActivity.java
+++ app/src/main/java/com/ygaps/demoactivity/MainActivity.java
@@ -5,11 +5,14 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,42 +29,50 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+        Log.d(TAG, "onCreate ...");
     }
 
     @Override
     protected void onStart() {
         super.onStart();
+        Log.d(TAG, "onStart ...");
     }
 
     @Override
     protected void onResume() {
         super.onResume();
+        Log.d(TAG, "onResume ...");
     }
 
     @Override
     protected void onPostResume() {
         super.onPostResume();
+        Log.d(TAG, "onPostResume ...");
     }
 
     @Override
     protected void onPause() {
         super.onPause();
+        Log.d(TAG, "onPause ...");
     }
 
 
     @Override
     protected void onStop() {
         super.onStop();
+        Log.d(TAG, "onStop ...");
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
+        Log.d(TAG, "onDestroy ...");
     }
 
     @Override
     protected void onRestart() {
         super.onRestart();
+        Log.d(TAG, "onRestart ...");
     }
 
     @Override
