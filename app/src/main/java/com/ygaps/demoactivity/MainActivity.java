09f0c6c394d9ea0c4015a7ff5b6c919d9a552d36
TPL <trphloc@gmail.com>
2017-05-15 20:10:38 +0700

    - viết các phương thức để các thể hiện các sự kiện chuyển trạng thái của activity

--- app/src/main/java/com/ygaps/demoactivity/MainActivity.java
+++ app/src/main/java/com/ygaps/demoactivity/MainActivity.java
@@ -29,6 +29,42 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPostResume() {
+        super.onPostResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+    }
+
+    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
